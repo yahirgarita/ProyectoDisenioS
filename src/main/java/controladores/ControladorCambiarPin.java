5f7264cc0927556d2de38ce77af27fda5e41a465
yahirgarita <73968209+yahirgarita@users.noreply.github.com>
2022-05-13 23:35:11 -0600

    Merge origin/master
    Conflicts:
    	pom.xml

--- src/main/java/controladores/ControladorCambiarPin.java
+++ src/main/java/controladores/ControladorCambiarPin.java
@@ -3,12 +3,15 @@ package controladores;
 import gui.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
 import java.util.Objects;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import logicadenegocios.*;
 import logicadeaccesoadatos.*;
 import util.*;
 import validaciones.*;
+import util.Email;
 /**
  * @author Carlos Rojas Molina
  * @author Jimmy Tsang Feng
@@ -19,10 +22,13 @@ import validaciones.*;
 public class ControladorCambiarPin implements ActionListener{
     public CambiarPinPaso1 cambiarPin1;
     public CambiarPinPaso2 cambiarPin2;
+    private int attempt = 0;
+    private Cuenta clienteActual;
     
     public ControladorCambiarPin(CambiarPinPaso1 pCambiarPin1, CambiarPinPaso2 pCambiarPin2){
         this.cambiarPin1 = pCambiarPin1;
         this.cambiarPin2 = pCambiarPin2;
+        this.clienteActual = null;
         this.cambiarPin1.volverPin1.addActionListener(this);
         this.cambiarPin1.continuarPin1.addActionListener(this);
         this.cambiarPin2.botonCambiarPin2.addActionListener(this);
@@ -64,9 +70,34 @@ public class ControladorCambiarPin implements ActionListener{
         CuentaBancaria cuentaBanc = CuentaBD.recuperarCuentaXNum(Encriptar.cifrar(this.cambiarPin2.jLabel4.getText()));
         if(Objects.equals(this.cambiarPin2.jLabel4.getText(),cuentaBanc.getPin())){
             if(ValidarTipoDeDato.validarFormatoPIN(this.cambiarPin2.pinActual.getText())){
-                
+                CuentaBD.cambiarPinCuenta(String.valueOf(cuentaBanc.getNumCuenta()), this.cambiarPin2.nuevoPin.getText());
+                Operacion operacion = new Operacion("modificación de Pin","Colones", false, 0, LocalDate.now());
+                OperacionBD.realizarOperacionEnBD(operacion, Encriptar.cifrar(this.cambiarPin2.jLabel4.getText()));
+                JOptionPane.showMessageDialog(null, "Su nuevo PIN ha sido almacenado exitosamente");
+                controladores.ControladoresGlobales.volver();
+                this.cambiarPin2.setVisible(false);
+                attempt = 0;
+            }else{
+                JOptionPane.showMessageDialog(null,"El formato del nuevo PIN que ingreso es incorrecto");
             }
+        }else{
+            JOptionPane.showMessageDialog(null, "El Pin que ingreso es erroneo, revise los datos ingresados");
+            attempt++;
+            comprobarIntentos(Encriptar.cifrar(String.valueOf(cuentaBanc.getNumCuenta())),this.cambiarPin2);
         }
     }
     
+    private void comprobrarIntentos(String pNumCuenta, JFrame frame, String pMsg){
+        
+        clienteActual = CuentaBD.recuperarCuentaXNum(Encriptar.cifrar(cambiarPin1.numCuentaPin.getText()));
+        Persona correoPersona = Persona(getCodigo().getText());
+        if(attempt == 2){
+            CuentaBD.modificarEstado(pNumCuenta, "Inactiva");
+            attempt = 0;
+            JOptionPane.showMessageDialog(null, pMsg);
+            Email.enviarEmail(clienteActual.getCorreoElectronico(), pMsg);
+            frame.dispose();
+            controladores.ControladoresGlobales.volver();
+        }
+    }
 }
