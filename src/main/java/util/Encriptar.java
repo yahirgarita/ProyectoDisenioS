284a60e97d3179165f013848bb61ab875dab39b8
yahirgarita <73968209+yahirgarita@users.noreply.github.com>
2022-05-18 19:57:49 -0600

    Merge origin/master
    Conflicts:
    	src/main/java/gui/Menu.form
    	src/main/java/gui/Menu.java

--- src/main/java/util/Encriptar.java
+++ src/main/java/util/Encriptar.java
@@ -7,7 +7,7 @@ package util;
  * 
  * @version 1.0
  */
-public class Encriptar {
+public interface Encriptar {
   public static String cifrar(String txtACifrar){
       StringBuilder msgCifrado = new StringBuilder();
       char[] caracter = txtACifrar.toCharArray();
